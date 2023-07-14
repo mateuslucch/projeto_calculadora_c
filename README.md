10c3934b850a9486d1a5bcdc66fadc60bd83621b
Mateus A Lucchese <mateuslucch@gmail.com>
2023-07-13 22:54:13 -0300

    Versão entregue

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Lab Project DIO - Desenvolvimento de uma Calculadora em Linguagem C
 
-## Projeto de uma calculadora com uso da linguagem C, para a formação Formação C/C++ Developer
+## Projeto de uma calculadora com uso da linguagem C, para a formação Formação C/C++ Developer da [DIO](https://web.dio.me/home)
 
-### Realizado com auxílio do CHATGTP
+- Feita no VSCode, no sistema Linux Mint.
+- Conversão binária e hexadecimal realizadas com auxílio do CHATGTP.
--- calculadora.c
+++ calculadora.c
@@ -1,4 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+long long decimalParaBinario(int decimalNum)
+{
+    long long binaryNum = 0;
+    int remainder, i = 1;
+
+    while (decimalNum > 0)
+    {
+        remainder = decimalNum % 2;
+        decimalNum = decimalNum / 2;
+        binaryNum += remainder * i;
+        i *= 10;
+    }
+
+    return binaryNum;
+}
+
+void decimalParaHexadecimal(int decimalNum, char hexadecimalNum[])
+{
+    int remainder;
+    int i = 0;
+
+    while (decimalNum > 0)
+    {
+        remainder = decimalNum % 16;
+        if (remainder < 10)
+            hexadecimalNum[i] = remainder + '0';
+        else
+            hexadecimalNum[i] = remainder - 10 + 'A';
+        decimalNum = decimalNum / 16;
+        i++;
+    }
+
+    int j, k;
+    char temp;
+    for (j = 0, k = i - 1; j < k; j++, k--)
+    {
+        temp = hexadecimalNum[j];
+        hexadecimalNum[j] = hexadecimalNum[k];
+        hexadecimalNum[k] = temp;
+    }
+
+    hexadecimalNum[i] = '\0';
+}
+
+void imprimirResultados(int resultado, char operador, int num1, int num2)
+{
+    char hexadecimalNum[50];
+    decimalParaHexadecimal(resultado, hexadecimalNum);
+
+    printf("\n");
+    printf("Resultados de %d %c %d em:\n", num1, operador, num2);
+    printf("Decimal: %d \n", resultado);
+    printf("Binário: %lld \n", decimalParaBinario(resultado));
+    printf("Hexadecimal: %s \n", hexadecimalNum);
+    printf("\n");
+}
 
 void Decimal()
 {
@@ -18,15 +76,15 @@ void Decimal()
     {
     case '+':
         resultado = num1 + num2;
-        printf("Resultado: %d\n", resultado);
+        imprimirResultados(resultado, operador, num1, num2);
         break;
     case '-':
         resultado = num1 - num2;
-        printf("Resultado: %d\n", resultado);
+        imprimirResultados(resultado, operador, num1, num2);
         break;
     case '*':
         resultado = num1 * num2;
-        printf("Resultado: %d\n", resultado);
+        imprimirResultados(resultado, operador, num1, num2);
         break;
     case '/':
         if (num2 == 0)
@@ -36,7 +94,7 @@ void Decimal()
         }
 
         resultado = num1 / num2;
-        printf("Resultado: %d\n", resultado);
+        imprimirResultados(resultado, operador, num1, num2);        
 
         break;
     default:
@@ -48,6 +106,5 @@ void Decimal()
 int main()
 {
     Decimal();
-
     return 0;
 }
